6f0802ea2802a20777bfffb8b4c6f2134d33cc71
Franck Pedron <franckpedrondev@gmail.com>
2023-11-30 16:45:14 +0100

    Feat - mise en page composant post-list-item

--- src/app/social-media/components/post-list-item/post-list-item.component.ts
+++ src/app/social-media/components/post-list-item/post-list-item.component.ts
@@ -1,11 +1,12 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Post} from "../../models/post.model";
+import {MatCardModule} from "@angular/material/card";
 
 @Component({
   selector: 'app-post-list-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './post-list-item.component.html',
   styleUrl: './post-list-item.component.scss'
 })
