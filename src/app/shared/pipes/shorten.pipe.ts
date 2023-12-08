d71283934c7432c9e5059f75757b0d3c09d536b3
Franck Pedron <franckpedrondev@gmail.com>
2023-12-08 17:05:40 +0100

    Feat - Création du pipe username

--- src/app/shared/pipes/shorten.pipe.ts
+++ src/app/shared/pipes/shorten.pipe.ts
@@ -1,12 +1,14 @@
 import {Pipe, PipeTransform} from "@angular/core";
 
-@Pipe({name: 'shorten',
-standalone: true,})
+@Pipe({
+  name: 'shorten',
+  standalone: true,
+})
 export class ShortenPipe implements PipeTransform {
-  transform(value: string): string {
-    if (value.length <=50) {
+  transform(value: string, maxLength: number = 50): string {
+    if (value.length <= maxLength) {
       return value;
     }
-    return value.substring(0, 50) + '...';
+    return value.substring(0, maxLength) + '...';
   }
 }
--- src/app/social-media/components/post-list-item/post-list-item.component.ts
+++ src/app/social-media/components/post-list-item/post-list-item.component.ts
@@ -4,11 +4,12 @@ import {Post} from "../../models/post.model";
 import {MatCardModule} from "@angular/material/card";
 import {CommentsComponent} from "../../../shared/components/comments/comments.component";
 import {ShortenPipe} from "../../../shared/pipes/shorten.pipe";
+import {UsernamePipe} from "../../../shared/pipes/username.pipe";
 
 @Component({
   selector: 'app-post-list-item',
   standalone: true,
-  imports: [CommonModule, MatCardModule, CommentsComponent, ShortenPipe],
+  imports: [CommonModule, MatCardModule, CommentsComponent, ShortenPipe, UsernamePipe],
   templateUrl: './post-list-item.component.html',
   styleUrl: './post-list-item.component.scss'
 })
@@ -19,6 +20,8 @@ export class PostListItemComponent implements OnInit {
 
   @Output()
   postCommented = new EventEmitter<{ comment: string, postId: number }>();
+
+  tempUser = {firstName: "Franck", lastName: "Pedron"};
 
   ngOnInit(): void {
   }
